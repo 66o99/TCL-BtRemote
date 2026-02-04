723117b531c25ecb6b4559578d9df820848484fc
66o99 <116540988+66o99@users.noreply.github.com>
2026-02-04 10:18:57 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-# plugins {
-#    alias(libs.plugins.android.application) apply false
-#    alias(libs.plugins.kotlin.compose) apply false
-# }
+
+// old-bak
+// plugins {
+//   alias(libs.plugins.android.application) apply false
+//   alias(libs.plugins.kotlin.compose) apply false
+// }
 
 android {
     // ... 其他配置 (compileSdk, defaultConfig 等)
