2fee47748bdfe5d9c36822f3e6f7021b40fea9b3
66o99 <116540988+66o99@users.noreply.github.com>
2026-02-04 10:27:12 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-// old-bak
-// plugins {
-//   alias(libs.plugins.android.application) apply false
-//   alias(libs.plugins.kotlin.compose) apply false
-// }
-
-buildTypes {
-        getByName("release") {
-            // 1. 赋值必须使用 '='
-            signingConfig = signingConfigs.getByName("debug")
-
-            // 2. 属性名需加 'is' 前缀，且使用 '=' 赋值
-            isMinifyEnabled = true
-            isShrinkResources = true
-            
-            // 3. 函数调用必须使用括号 '()'，且字符串使用双引号 ""
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
+ plugins {
+   alias(libs.plugins.android.application) apply false
+   alias(libs.plugins.kotlin.compose) apply false
+ }
